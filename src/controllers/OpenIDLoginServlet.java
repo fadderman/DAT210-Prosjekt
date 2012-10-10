d21bbf11f8c01d871e28a2533c6ecf5868785988
Thomas Hinna <thomashinna@gmail.com>
2012-10-10 10:39:07 +0200

    Revert "removed unused import from openIDloginServlet"
    This reverts commit f23b74426775f31a4c01385d3f3235f7021ac638.

--- src/controllers/OpenIDLoginServlet.java
+++ src/controllers/OpenIDLoginServlet.java
@@ -3,6 +3,8 @@ package controllers;
 import java.io.IOException;
 import java.util.List;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -25,6 +27,7 @@ import org.openid4java.message.ax.AxMessage;
 import org.openid4java.message.ax.FetchRequest;
 import org.openid4java.message.ax.FetchResponse;
 
+import business.openid.UserLogin;
 import business.user.UserHandler;
 
 public class OpenIDLoginServlet extends HttpServlet{
