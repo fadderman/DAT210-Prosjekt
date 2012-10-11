f2bb2ac2c0801c5cb4a98cd2b761db65e74e5f42
fadderman <g.falnes@stud.uis.no>
2012-10-11 20:08:33 +0200

    started on the logic in SeekMentorWithGivenSubject

--- src/business/SeekMentorsWithGivenSubject.java
+++ src/business/SeekMentorsWithGivenSubject.java
@@ -1,5 +1,26 @@
 package business;
 
-public class SeekMentorsWithGivenSubject {
+import java.util.LinkedList;
 
+import models.User;
+
+public class SeekMentorsWithGivenSubject<E> implements Comparable<E> {
+	
+	//skal bruken en metode fra Controlleren får å få inn i hvilket faget mentor bli søkt i
+	//tom for nå
+	private String subject = "";
+	//skal bruk en metode fra usermanagement for å få
+	private LinkedList<User> mentors;
+	
+	
+	
+	
+	
+	
+	@Override
+	public int compareTo(E object) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
 }
