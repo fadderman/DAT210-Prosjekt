5c5b117f76bb49e165d80fa5d53495aa787076b7
fadderman <g.falnes@stud.uis.no>
2012-10-12 12:28:24 +0200

    Created a JUnit test for SeekUsersWithGivenSubject

---
+++ test/business/test/SUWGSTest.java
@@ -0,0 +1,14 @@
+package business.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SUWGSTest {
+
+	@Test
+	public void testSeekUsersWithGivenSubject() {
+		fail("Not yet implemented");
+	}
+
+}
