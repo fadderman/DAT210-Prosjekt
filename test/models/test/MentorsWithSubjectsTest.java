1319dc01c1d9a93dce55f510e843c8552ec28042
fadderman <g.falnes@stud.uis.no>
2012-10-12 23:57:12 +0200

    messed around with tests

--- test/models/test/MentorsWithSubjectsTest.java
+++ test/models/test/MentorsWithSubjectsTest.java
@@ -2,10 +2,26 @@ package models.test;
 
 import static org.junit.Assert.*;
 
+import java.util.LinkedList;
+
+import models.User;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class MentorsWithSubjectsTest {
+	private LinkedList<User> mentors;
 
+	@Before
+	public void setUp() throws Exception {
+		mentors = new LinkedList<User>();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		mentors = null;
+	}
 	@Test
 	public void test() {
 		fail("Not yet implemented");
