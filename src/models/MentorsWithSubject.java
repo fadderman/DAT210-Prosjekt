0663a5a1515c18fd319855952b557029ef22e10f
fadderman <g.falnes@stud.uis.no>
2012-10-15 16:58:44 +0200

    Model and business class are finished

--- src/models/MentorsWithSubject.java
+++ src/models/MentorsWithSubject.java
@@ -1,5 +1,22 @@
 package models;
 
-public class MentorsWithSubject {
+import java.util.LinkedList;
 
+import business.SeekUsersWithGivenSubject;
+
+public class MentorsWithSubject {
+	private LinkedList<User> mentorsWithSubject;
+	
+	public MentorsWithSubject(String subject){
+		mentorsWithSubject = new SeekUsersWithGivenSubject(subject).getMentorsWithSbject();
+	}
+
+	public LinkedList<User> getMentorsWithSubject() {
+		return mentorsWithSubject;
+	}
+
+	public void setMentorsWithSubject(LinkedList<User> mentorsWithSubject) {
+		this.mentorsWithSubject = mentorsWithSubject;
+	}
+	
 }
