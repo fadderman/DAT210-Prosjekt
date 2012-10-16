3f5ff53a26bc576c885d58642148134209a84770
Bartosz Wasilewski <arnie.governator@gmail.com>
2012-10-16 13:48:08 +0200

    started work on ConnectionManagementTest

--- test/hibernate/test/ConnectionManagementTest.java
+++ test/hibernate/test/ConnectionManagementTest.java
@@ -1,15 +1,31 @@
 package hibernate.test;
 
 import static org.junit.Assert.*;
+import hibernate.CommentManagement;
+import hibernate.ConnectionManagement;
+import hibernate.FieldManagement;
+import hibernate.SubjectManagement;
+import hibernate.UserManagement;
 
 import org.junit.Before;
 import org.junit.Test;
 
 public class ConnectionManagementTest {
 
+	private CommentManagement cm;
+	private ConnectionManagement xm;
+	private FieldManagement fm;
+	private SubjectManagement sm;
+	private UserManagement um;
+
 	@Before
 	public void setUp() throws Exception {
 		
+		cm = new CommentManagement();
+		xm = new ConnectionManagement();
+		fm = new FieldManagement();
+		sm = new SubjectManagement();
+		um = new UserManagement();
 	}
 
 	@Test
