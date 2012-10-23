5868d76033ad5af3c9067e23d0d156b5846b7032
Stephan Svendson <lappeteppet@gmail.com>
2012-10-23 09:39:38 +0200

    Progress on CommentManagementTest

--- test/hibernate/test/CommentManagementTest.java
+++ test/hibernate/test/CommentManagementTest.java
@@ -7,26 +7,43 @@ import java.util.List;
 
 import hibernate.*;
 
-import models.Comment;
+import models.*;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
+
 
 public class CommentManagementTest {
 	
-	private CommentManagement cm;
-	private ConnectionManagement xm;
-	private SubjectManagement sm;
-	private FieldManagement fm;
-	private UserManagement um;
+	private static CommentManagement cm;
+	private static ConnectionManagement xm;
+	private static SubjectManagement sm;
+	private static FieldManagement fm;
+	private static UserManagement um;
 	
-	@Before
-	public void setUp() throws Exception{
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception{
 		cm = new CommentManagement();
 		xm = new ConnectionManagement();
 		fm = new FieldManagement();
 		sm = new SubjectManagement();
 		um = new UserManagement();
+		
+		
+		
+		sm.createSubject("Programming", "Programming related");
+		sm.createSubject("Non-programming", "not programming related");
+		
+		fm.createField("Java", "This is Java", sm.getSingleByTitle("Programming"));
+		fm.createField("C++", "C plus plus", sm.getSingleByTitle("Programming"));
+		fm.createField("Google tips", "Google like a pro", sm.getSingleByTitle("Non-programming"));
+		fm.createField("Bathroom Wall", "How to copy code from the internet", sm.getSingleByTitle("Non-programming"));
+		fm.createField("COBOL", "Old stuff for old ppl", sm.getSingleByTitle("Programming"));
+		
+		um.createUser("John", "First", "johnfirst@gmail.com", "Here", "It exits on Earth", "numero uno");
+		um.createUser("Bob", "Second", "bob.second@outlook.com", "Not far away", "Ground", "numero dos");
+		um.createUser("Bob", "Last", "phil@last.com", "Here", "It exits on Earth", "numero tres");
+		um.createUser("Some", "Guy", "someguy@somewhere.com", "Somewhere", "Place", "numero quatro");
+		
 		
 		
 //		xm.createConnection(mentor, trainee, field, difficultyLevel)
@@ -35,19 +52,24 @@ public class CommentManagementTest {
 //		
 	}
 	
-	
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	@Before
+	public void setUp(){
+		
 	}
+
+	
+	
+//	@Test
+//	public void test() {
+//		fail("Not yet implemented");
+//	}
 	
 	@Test
 	public void testCreateComment(){
-//		assertEquals(cm.createComment(connection, author, text), true);
-//		assertEquals(cm.createComment(connection, author, text), true);
-//		assertEquals(cm.createComment(connection, author, text), true);
-//		assertEquals(cm.createComment(connection, author, text), true);
+		assertEquals(cm.createComment(xm.getByID(0), um.getByName("John", "First").get(0), "This is a text"), true);
+		assertEquals(cm.createComment(xm.getByID(1), um.getByEmail("johnfirst@gmail.com"), "This is some text"), true);
+		assertEquals(cm.createComment(xm.getByID(3), um.getByEmail("bob.second@outlook.com"), "Some random text"), true);
+		assertEquals(cm.createComment(xm.getByID(2), um.getByEmail("someguy@somewhere.com"), "Comment about programming"), true);
 		
 	}
 	
