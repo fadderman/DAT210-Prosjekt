05333ec3f1f62bee457c3b0c42e7ff8d6970d5c4
fadderman <g.falnes@stud.uis.no>
2012-10-23 16:28:57 +0200

    still more old classes gotten rid of

--- src/business/user/UserHandler.java
+++ src/business/user/UserHandler.java
@@ -8,29 +8,51 @@ import models.User;
 
 public class UserHandler {
 	
-	private static ArrayList<User> users;		//holds all the users in the system
+	private static ArrayList<User> userList= new ArrayList<User>();
 	private static UserManagement userManager;		//access to the database
 	
 	public UserHandler(){
-		users = new ArrayList<User>();
+//		users = new ArrayList<User>();
 //		userManager = new UserManagement();
 	}
 
-	public void addUser(User user) {
-		synchronized (users) {
-			users.add(user);
+	public static void addUser(User user) {
+		synchronized (userList) {
+			userList.add(user);
 		}
 	}
 
-	public User getUserByIdentifier(String identifier) {
+	public static User getUserByIdentifier(String identifier) {
 		if(identifier.isEmpty())return null;
 		
-		for(int i=0; i<users.size();i++){
-			if(users.get(i).getIdentifier().equals(identifier)){
-				return users.get(i);
+		for(int i=0; i<userList.size();i++){
+			if(userList.get(i).getIdentifierOpenID().equals(identifier)){
+				return userList.get(i);
 			}
 		}
 		return null;
 	}
+	
+	public static User getUserByIndex(int index){
+		return userList.get(index);
+	}
+	
+	public static int getUserListSize(){
+		return userList.size();
+	}
+	
+//	public static ArrayList<User> getUsersByID(ArrayList<UserSuggestion> usersToGet){
+//		ArrayList<User> usersToReturn = new ArrayList<User>();
+//		for(int i=0;i<userList.size();i++){
+//			for(int j=0;j<usersToGet.size();j++){
+//				if(usersToGet.get(j).getUserID() == userList.get(i).getUserID()){
+//					usersToReturn.add(userList.get(i));
+//					usersToGet.remove(j);
+//				}
+//			}			
+//		}
+//		return usersToReturn;
+//	}
+	
 	
 }
