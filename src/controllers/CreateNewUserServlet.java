d8a574eb5dfdc1ec5fa4cc10b8bd98425e449383
lordalek <alekspett@gmail.com>
2012-10-23 10:33:47 +0200

    testing JSPTestFramework

--- src/controllers/CreateNewUserServlet.java
+++ src/controllers/CreateNewUserServlet.java
@@ -46,16 +46,19 @@ public class CreateNewUserServlet extends HttpServlet {
 
 		ConnectionHandler connectionHandler = new ConnectionHandler();
 		Connection connection;
-		
+		for(int i = 1; i < field.length; i++){
+			connection = createConnection(user, field[i], description[i], experience[i], traineeRadioButton[i], mentorRadioButton[i]);
+			connectionHandler.addConnection(connection);
+		}
 
-			String url = "/home.jsp";
+		String url = "/home.jsp";
 
-			RequestDispatcher dispatcher = getServletContext()
-					.getRequestDispatcher(url);
-			dispatcher.forward(request, response);
+		RequestDispatcher dispatcher = getServletContext()
+				.getRequestDispatcher(url);
+		dispatcher.forward(request, response);
 	}
 
-	private User createNewUser(String firstName, String lastName, String email, String city, String country, String identifier){
+	public User createNewUser(String firstName, String lastName, String email, String city, String country, String identifier){
 		User user = new User(firstName, lastName, email, city, country, identifier);
 		String mapURL = new String("http://maps.googleapis.com/maps/api/staticmap?center=");
 		mapURL += city + ",";
@@ -63,50 +66,44 @@ public class CreateNewUserServlet extends HttpServlet {
 		user.setMapURL(mapURL);
 		return user;
 	}
-	
-	private void createConnection(User user, String[] field, String[] description, String[] experience, String[] traineeRadioButton, String[] mentorRadioButton){
-		for(int i = 1; i < field.length; i++){
-			if(traineeRadioButton[i-1].equals("Yes")){
-				if(experience[i-1].equalsIgnoreCase("expert")){
-					connection = new Connection(new User("null"), user, new Field(field[i]), description[i], 2);
-					connectionHandler.addConnection(connection);
-				}
-				if(experience[i-1].equalsIgnoreCase("intermediate")){
-					connection = new Connection(new User("null"), user, new Field(field[i]), description[i], 1);
-					connectionHandler.addConnection(connection);
-				}
-				if(experience[i-1].equalsIgnoreCase("novice")){
-					connection = new Connection(new User("null"), user, new Field(field[i]), description[i], 0);
-					connectionHandler.addConnection(connection);
-				}
+
+	public Connection createConnection(User user, String field, String description, String experience, String traineeRadioButton, String mentorRadioButton){
+		Connection connection = null;
+		if(traineeRadioButton.equals("Yes")){
+			if(experience.equalsIgnoreCase("expert")){
+				connection = new Connection(new User("null"), user, new Field(field), description, 2);
 			}
-			if(mentorRadioButton[i-1].equals("Yes")){
-				if(experience[i-1].equalsIgnoreCase("expert")){
-					connection = new Connection(user, new User("null") , new Field(field[i]), description[i], 2);
-					connectionHandler.addConnection(connection);
-				}
-				if(experience[i-1].equalsIgnoreCase("intermediate")){
-					connection = new Connection(user, new User("null"), new Field(field[i]), description[i], 1);
-					connectionHandler.addConnection(connection);
-				}
-				if(experience[i-1].equalsIgnoreCase("novice")){
-					connection = new Connection(user, new User("null"), new Field(field[i]), description[i], 0);
-					connectionHandler.addConnection(connection);
-				}
+			else if(experience.equalsIgnoreCase("intermediate")){
+				connection = new Connection(new User("null"), user, new Field(field), description, 1);
 			}
-			if((traineeRadioButton[i-1].equals("Yes") && mentorRadioButton[i-1].equals("Yes")) || (traineeRadioButton[i-1].equals("No") && mentorRadioButton[i-1].equals("No") )){
-				if(experience[i-1].equalsIgnoreCase("expert")){
-					connection = new Connection(new User("null"), user, new Field(field[i]), description[i], 2);
-					connectionHandler.addConnection(connection);
-				}
-				if(experience[i-1].equalsIgnoreCase("intermediate")){
-					connection = new Connection(new User("null"), user, new Field(field[i]), description[i], 1);
-					connectionHandler.addConnection(connection);
-				}
-				if(experience[i-1].equalsIgnoreCase("novice")){
-					connection = new Connection(new User("null"), user, new Field(field[i]), description[i], 0);
-					connectionHandler.addConnection(connection);
-				}
+			else if(experience.equalsIgnoreCase("novice")){
+				connection = new Connection(new User("null"), user, new Field(field), description, 0);
+			}
+		}
+		if(mentorRadioButton.equals("Yes")){
+			if(experience.equalsIgnoreCase("expert")){
+				connection = new Connection(user, new User("null") , new Field(field), description, 2);
+			}
+			else if(experience.equalsIgnoreCase("intermediate")){
+				connection = new Connection(user, new User("null"), new Field(field), description, 1);
+			}
+			else if(experience.equalsIgnoreCase("novice")){
+				connection = new Connection(user, new User("null"), new Field(field), description, 0);
+			}
+		}
+		if((traineeRadioButton.equals("Yes") && mentorRadioButton.equals("Yes")) || (traineeRadioButton.equals("No") && mentorRadioButton.equals("No") )){
+			if(experience.equalsIgnoreCase("expert")){
+				connection = new Connection(new User("null"), user, new Field(field), description, 2);
+			}
+			else if(experience.equalsIgnoreCase("intermediate")){
+				connection = new Connection(new User("null"), user, new Field(field), description, 1);
+
+			}
+			else if(experience.equalsIgnoreCase("novice")){
+				connection = new Connection(new User("null"), user, new Field(field), description, 0);
+
+			}
+		}
+		return connection;
 	}
-	
 }
--- test/controllers/firstTimeLogin/test/firstTimeLoginTest.java
+++ test/controllers/firstTimeLogin/test/firstTimeLoginTest.java
@@ -2,24 +2,33 @@ package controllers.firstTimeLogin.test;
 
 import static org.junit.Assert.*;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.Servlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import models.User;
 
+import org.apache.catalina.connector.Request;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.meterware.servletunit.ServletTestCase;
+
 import controllers.CreateNewUserServlet;
 
-public class firstTimeLoginTest {
+public class firstTimeLoginTest extends JspTestCase{
 
+	public void beginCreateNewUser(){
+		RequestDispatcher rd = theConfig.getServletContext().getRequestDispatcher("")
+	}
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 		User mentor = new User("Bob", "Saget", "bob@saget.com", "LA", "US", "1234");
 		User trainee = new User("Joe", "Capri", "joe@capri.com", "SA", "US", "9876");
-		CreateNewUserServlet servlet = new CreateNewUserServlet();
+		
+		
 		
 	}
 
