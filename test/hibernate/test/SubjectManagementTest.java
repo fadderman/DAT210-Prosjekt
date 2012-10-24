55b9f0ca0a45674e2a94875232951898ce3cdcc4
Stephan Svendson <lappeteppet@gmail.com>
2012-10-24 11:00:53 +0200

    Progress on CommentManagementTest

--- test/hibernate/test/SubjectManagementTest.java
+++ test/hibernate/test/SubjectManagementTest.java
@@ -15,12 +15,12 @@ public class SubjectManagementTest {
 	ConnectionManagement xm;
 	FieldManagement fm;
 	UserManagement um;
-	
-	
-	
 
-	
-	
+
+
+
+
+
 	@Before
 	public void beforeTest() throws Exception{
 		sm = new SubjectManagement();
@@ -28,101 +28,101 @@ public class SubjectManagementTest {
 		xm = new ConnectionManagement();
 		fm = new FieldManagement();
 		um = new UserManagement();
-		
+
 		fm.createField("Java", "This is java", null);
 		sm.createSubject("Programming", "Programming related");
 		sm.createSubject("Non-programming", "not programming related");
-		
 
-		
+
+
 	}
-	
+
 
 
 	@Test
 	public void testCreateSubject() {
-		
-//		Subject subject1 = new Subject("Java3D", "Programming language");
-//		Subject subject2 = new Subject("C#", "Old programming language");
-//		Subject subject3 = new Subject("Java", "Awsomeness programming language");
-		
-		
 
-		
-		
+		//		Subject subject1 = new Subject("Java3D", "Programming language");
+		//		Subject subject2 = new Subject("C#", "Old programming language");
+		//		Subject subject3 = new Subject("Java", "Awsomeness programming language");
+
+
+
+
+
 		assertEquals(sm.createSubject("Java3D", "Programming language"), true);
 		assertEquals(sm.createSubject("C#", "Old Programming language"), true);
 		assertEquals(sm.createSubject("Java", "Awsomeness programming language"), true);
-//		
-//		assertEquals(sm.getByTitle("Java3D").get(0).getDescription(), "Programming language");
-//		assertEquals(sm.getByTitle("C#").get(0).getDescription(), "Programming language");
-//		assertEquals(sm.getByTitle("Java").get(0).getDescription(), "Awsomeness programming language");
-		
-		
-		
+		//		
+		//		assertEquals(sm.getByTitle("Java3D").get(0).getDescription(), "Programming language");
+		//		assertEquals(sm.getByTitle("C#").get(0).getDescription(), "Programming language");
+		//		assertEquals(sm.getByTitle("Java").get(0).getDescription(), "Awsomeness programming language");
+
+
+
 	}
-	
+
 	@Test
 	public void testGetAllSubjects(){
 		List<Subject> list = sm.getAllSubjects();
-		
+
 		for (Iterator<Subject> iterator = list.iterator(); iterator.hasNext();) {
-//			Subject current = iterator.next();
+			//			Subject current = iterator.next();
 			assertEquals(iterator.next().getClass(), Subject.class);
 		}
-		
+
 	}
-	
+
 	@Test
 	public void testGetByID(){
 		assertEquals(sm.getByID(1).getTitle(), "Programming");
 	}
-	
 
-	
+
+
 	@Test
 	public void testGetByTitle(){
 		List<Subject> list = sm.getByTitle("Java3D");
 		assertEquals(list.get(0).getDescription(), "Programming language");
 	}
-	
+
 	@Test
 	public void testGetSingleByTitle(){
 		Subject s = sm.getSingleByTitle("Java3D");
 		assertEquals(s.getTitle(), "Java3D");
 	}
-	
 
-	
+
+
 	@Test
 	public void testUpdateTitle(){
 		List<Subject> test = sm.getByTitle("Java3D");
 		sm.updateTitle( test.get(0), "Java4D");
 		assertEquals(sm.getByTitle("Java4D").get(0).getTitle(), "Java4D");
-		
+
 	}
-	
+
 	@Test
 	public void testUpdateDescription(){
 		List<Subject> test = sm.getByTitle("Programming");
 		sm.updateDescription(test.get(0), "Programming for pros!!!!");
 		assertEquals(sm.getByTitle("Programming").get(0).getDescription(), "Programming for pros!!!!");
-}
-	
+	}
+
 	@Test
 	public void testChangeStatus(){
 		Subject test = sm.getByID(3);
 		sm.changeStatus(test, false);
-		
+
 		List<Subject> list = sm.getAllInactiveSubjects();
-		
+
 		for(Iterator<Subject> iterator = list.iterator(); iterator.hasNext();){
 			Subject current = iterator.next();
 			System.out.println(current.getTitle());
 			assertEquals(current.isActive(), false);
 		}
 	}
-	
+
 	@Test
 	public void testFetchFieldList(){
 		List<Field> list = sm.fetchFieldList(sm.getSingleByTitle("Java"));
@@ -132,9 +132,9 @@ public class SubjectManagementTest {
 		}
 		assertEquals(list.size(), 0);
 	}
-		
-		
-	}
+
+
+}
 //		assertEquals(fm.createField("Java", "This is Java", sm.getSingleByTitle("Programming")), true);
 //		assertEquals(fm.createField("C++", "C plus plus", sm.getSingleByTitle("Programming")), true);
 //		assertEquals(fm.createField("Google tips", "Google like a pro", sm.getSingleByTitle("Non-programming")), true);
@@ -149,13 +149,13 @@ public class SubjectManagementTest {
 //			Subject current = i.next();
 //			System.out.println(current.getTitle()); 
 //		}	}
-	
-	
-	
-	
-	//TODO List<Field> fetchFieldList
-	
-	
+
+
+
+
+//TODO List<Field> fetchFieldList
+
+
 
 
 
