e484d3d9124f6061ec2dd458bdb30cd7ee8a142e
fadderman <g.falnes@stud.uis.no>
2012-11-02 16:55:26 +0100

    started on the shell of the servlet

--- src/controllers/RegisterConnection.java
+++ src/controllers/RegisterConnection.java
@@ -1,6 +1,11 @@
 package controllers;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class RegisterConnection extends HttpServlet {
 
@@ -9,5 +14,15 @@ public class RegisterConnection extends HttpServlet {
 	 */
 	private static final long serialVersionUID = 1L;
 
+	public RegisterConnection(){
+		
+	}
 	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
 }
