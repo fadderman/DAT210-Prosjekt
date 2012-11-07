f3d527010cdc7d9136e6357b4f15fd5a89edbae2
Glenn Falnes <fadderman@gmail.com>
2012-11-07 11:23:30 +0100

    finished adding all that github deleted by being dumb

--- src/controllers/RegisterConnection.java
+++ src/controllers/RegisterConnection.java
@@ -1,5 +1,9 @@
 package controllers;
 
+import hibernate.ConnectionManagement;
+import hibernate.FieldManagement;
+import hibernate.UserManagement;
+
 import java.io.IOException;
 
 import javax.servlet.ServletException;
@@ -8,7 +12,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import models.Field;
+import models.User;
+
 public class RegisterConnection extends HttpServlet {
+ 	private static UserManagement um = new UserManagement();
+	private static ConnectionManagement cm = new ConnectionManagement();
+	private static FieldManagement fm = new FieldManagement();
 
 	/**
 	 * placeholder serialnumber
@@ -16,22 +26,22 @@ public class RegisterConnection extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	public RegisterConnection(){
-		
+
 	}
-	
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		createConnection(request, response);
+		createConnectionWithMentor(request, response);
 	}
-	
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		createConnection(request, response);
+		createConnectionWithMentor(request, response);
 	}
-	
-	private static void createConnection(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	private static void createConnectionWithMentor(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		Cookie[] cookies = request.getCookies();
 		String cookiename = "user_id";
 		String cookievalue = "";
-		
+
 		for(int i = 0; i < cookies.length; i++){
 			Cookie cookie = cookies[i];
 			if(cookiename.equals(cookie.getName())){
@@ -39,5 +49,18 @@ public class RegisterConnection extends HttpServlet {
 			}
 		}
 		int userID = Integer.parseInt(cookievalue);
+
+		int traineeUserID = Integer.parseInt(cookievalue);
+		User trainee = um.getByID(traineeUserID);
+
+		String formUserID = request.getParameter("userID");
+		int mentorUserID = Integer.parseInt(formUserID);
+		User mentor = um.getByID(mentorUserID);
+
+		String fieldstring = request.getParameter("field");
+		Field field = (Field) fm.getByTitle(fieldstring);
+
+		cm.createConnection(mentor, trainee, field);
+
 	}
 }
